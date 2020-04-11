f1eba8b7def017b7d5282ede044d5f9bb6d96fb0
Nikita Shikuts <shikutsn@bk.ru>
2020-04-11 20:22:03 +0300

    Обновит названия свойств объекта фильтр в соответствии с заданием

--- src/mock/filter.js
+++ src/mock/filter.js
@@ -5,7 +5,7 @@ const filterNames = [
 const generateFilters = () => {
   return filterNames.map((it) => {
     return {
-      name: it,
+      title: it,
       count: Math.floor(Math.random() * 10),
     };
   });
