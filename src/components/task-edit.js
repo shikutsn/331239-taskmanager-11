967fbe5cf49299d881a768a25abe2e12035198b0
Nikita Shikuts <shikutsn@bk.ru>
2020-04-11 17:05:51 +0300

    Добавит логику генерации разметки на основе `repeatingDays`

--- src/components/task-edit.js
+++ src/components/task-edit.js
@@ -1,82 +1,25 @@
-import {MONTH_NAMES} from "../const.js";
+import {DAYS, MONTH_NAMES} from "../const.js";
 import {formatTime} from "../utils.js";
 
-const createRepeatingDaysMarkup = () => {
-  return (
-    `<input
-      class="visually-hidden card__repeat-day-input"
-      type="checkbox"
-      id="repeat-mo-4"
-      name="repeat"
-      value="mo"
-    />
-    <label class="card__repeat-day" for="repeat-mo-4"
-      >mo</label
-    >
-    <input
-      class="visually-hidden card__repeat-day-input"
-      type="checkbox"
-      id="repeat-tu-4"
-      name="repeat"
-      value="tu"
-      checked
-    />
-    <label class="card__repeat-day" for="repeat-tu-4"
-      >tu</label
-    >
-    <input
-      class="visually-hidden card__repeat-day-input"
-      type="checkbox"
-      id="repeat-we-4"
-      name="repeat"
-      value="we"
-    />
-    <label class="card__repeat-day" for="repeat-we-4"
-      >we</label
-    >
-    <input
-      class="visually-hidden card__repeat-day-input"
-      type="checkbox"
-      id="repeat-th-4"
-      name="repeat"
-      value="th"
-    />
-    <label class="card__repeat-day" for="repeat-th-4"
-      >th</label
-    >
-    <input
-      class="visually-hidden card__repeat-day-input"
-      type="checkbox"
-      id="repeat-fr-4"
-      name="repeat"
-      value="fr"
-      checked
-    />
-    <label class="card__repeat-day" for="repeat-fr-4"
-      >fr</label
-    >
-    <input
-      class="visually-hidden card__repeat-day-input"
-      type="checkbox"
-      name="repeat"
-      value="sa"
-      id="repeat-sa-4"
-    />
-    <label class="card__repeat-day" for="repeat-sa-4"
-      >sa</label
-    >
-    <input
-      class="visually-hidden card__repeat-day-input"
-      type="checkbox"
-      id="repeat-su-4"
-      name="repeat"
-      value="su"
-      checked
-    />
-    <label class="card__repeat-day" for="repeat-su-4"
-      >su</label
-    >`
-  );
+const createRepeatingDaysMarkup = (days, repeatingDays) => {
+  return days
+    .map((day, index) => {
+      const isChecked = repeatingDays[day];
+      return (
+        `<input
+          class="visually-hidden card__repeat-day-input"
+          type="checkbox"
+          id="repeat-${day}-${index}"
+          name="repeat"
+          value="${day}"
+          ${isChecked ? `checked` : ``}
+        />
+        <label class="card__repeat-day" for="repeat-${day}-${index}"
+          >${day}</label
+        >`
+      );
+    })
+    .join(`\n`);
 };
 
 const createColorsMarkup = () => {
@@ -154,11 +97,12 @@ export const createTaskEditTemplate = (task) => {
   const date = isDateShowing ? `${dueDate.getDate()} ${MONTH_NAMES[dueDate.getMonth()]}` : ``;
   const time = isDateShowing ? formatTime(dueDate) : ``;
 
-  const repeatClass = `card--repeat`;
+  const isRepeatingTask = Object.values(repeatingDays).some(Boolean);
+  const repeatClass = isRepeatingTask ? `card--repeat` : ``;
   const deadlineClass = isExpired ? `card--deadline` : ``;
 
   const colorsMarkup = createColorsMarkup();
-  const repeatingDaysMarkup = createRepeatingDaysMarkup();
+  const repeatingDaysMarkup = createRepeatingDaysMarkup(DAYS, repeatingDays);
 
   return (
     `<article class="card card--edit card--${color} ${repeatClass} ${deadlineClass}">
@@ -200,13 +144,17 @@ export const createTaskEditTemplate = (task) => {
       : ``
     }
                 <button class="card__repeat-toggle" type="button">
-                  repeat:<span class="card__repeat-status">yes</span>
+                  repeat:<span class="card__repeat-status">${isRepeatingTask ? `yes` : `no`}</span>
                 </button>
-                <fieldset class="card__repeat-days">
-                  <div class="card__repeat-days-inner">
-                    ${repeatingDaysMarkup}
-                  </div>
-                </fieldset>
+                ${
+    isRepeatingTask ?
+      `<fieldset class="card__repeat-days">
+                      <div class="card__repeat-days-inner">
+                        ${repeatingDaysMarkup}
+                      </div>
+                    </fieldset>`
+      : ``
+    }
               </div>
             </div>
             <div class="card__colors-inner">
