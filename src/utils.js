cf48d21a52c5ee5cd117ae82947f6cedd4565488
Nikita Shikuts <shikutsn@bk.ru>
2020-04-15 06:20:10 +0300

    Добавит вспомогательную функцию для создания DOM-элемента

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,15 @@
-export const formatTime = (date) => {
+const formatTime = (date) => {
   const hours = String(date.getHours()).padStart(2, `0`);
   const minutes = String(date.getMinutes()).padStart(2, `0`);
 
   return `${hours}:${minutes}`;
 };
+
+const createElement = (template) => {
+  const newElement = document.createElement(`div`);
+  newElement.innerHTML = template;
+
+  return newElement.firstChild;
+};
+
+export {formatTime, createElement};
