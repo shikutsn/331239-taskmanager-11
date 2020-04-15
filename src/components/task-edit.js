5c757302db8668f506abc14ebeddf697b605273c
Nikita Shikuts <shikutsn@bk.ru>
2020-04-15 06:35:39 +0300

    Перепишет на классы оставшиеся компоненты. WIP

--- src/components/task-edit.js
+++ src/components/task-edit.js
@@ -1,5 +1,5 @@
 import {COLORS, DAYS, MONTH_NAMES} from "../const.js";
-import {formatTime} from "../utils.js";
+import {createElement, formatTime} from "../utils.js";
 
 const createRepeatingDaysMarkup = (days, repeatingDays) => {
   return days
@@ -44,7 +44,7 @@ const createColorsMarkup = (colors, currentColor) => {
     .join(`\n`);
 };
 
-export const createTaskEditTemplate = (task) => {
+const createTaskEditTemplate = (task) => {
   const {description, dueDate, color, repeatingDays} = task;
 
   const isExpired = dueDate instanceof Date && dueDate < Date.now();
@@ -96,9 +96,7 @@ export const createTaskEditTemplate = (task) => {
                           value="${date} ${time}"
                         />
                       </label>
-                    </fieldset>`
-      : ``
-    }
+                    </fieldset>` : `` }
                 <button class="card__repeat-toggle" type="button">
                   repeat:<span class="card__repeat-status">${isRepeatingTask ? `yes` : `no`}</span>
                 </button>
@@ -108,9 +106,7 @@ export const createTaskEditTemplate = (task) => {
                       <div class="card__repeat-days-inner">
                         ${repeatingDaysMarkup}
                       </div>
-                    </fieldset>`
-      : ``
-    }
+                    </fieldset>` : `` }
               </div>
             </div>
             <div class="card__colors-inner">
@@ -129,3 +125,26 @@ export const createTaskEditTemplate = (task) => {
     </article>`
   );
 };
+
+export default class TaskEdit {
+  constructor(task) {
+    this._task = task;
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createTaskEditTemplate(this._task);
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
